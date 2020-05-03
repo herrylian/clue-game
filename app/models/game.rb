d01915cc4f988a1fe0e336c53bcc76826cc5fbbd
herry <herry.lian@gmail.com>
2020-05-03 11:57:01 -0700

    set identity on client

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,7 +5,13 @@ class Game < ApplicationRecord
 
     # Broadcast back to the players subscribed to the channel that the game has started
     ActionCable.server.broadcast "player_#{cross}", {msg: "Cross", action: "hide"}
-    ActionCable.server.broadcast "player_#{nought}", {msg: "Nought", action: "none"}
+    ActionCable.server.broadcast "player_#{nought}", {msg: "Nought", action: "hide"}
+
+    # Send UUID to client
+    ActionCable.server.broadcast "player_#{cross}", {msg: player1, action: "set_identity"}
+    ActionCable.server.broadcast "player_#{nought}", {msg: player2, action: "set_identity"}
+
+    ActionCable.server.broadcast "player_#{nought}", {msg: "Nought", action: "unhide"}
 
     # Store the details of each opponent
     # REDIS.set("opponent_for:#{cross}", nought)
